82d37cee0a83bdeb9f1df72e9de1375e564ff3c9
Isaac Caruso <icaruso21@amherst.edu>
2021-05-06 20:59:11 -0400

    project setup

---
+++ src/TriestBase.java
@@ -0,0 +1,13 @@
+public class TriestBase implements DataStreamAlgo {
+    /*
+     * Constructor.
+     * The parameter samsize denotes the size of the sample, i.e., the number of
+     * edges that the algorithm can store.
+     */
+	public TriestBase(int samsize) {
+	}
+
+	public void handleEdge(Edge edge) {}
+
+	public int getEstimate() { return 0; } // You shouldn't return 0 ;-)
+}
